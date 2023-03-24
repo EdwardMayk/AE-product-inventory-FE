b4efa698aad31ecf9f925cbda8aa769f9f86d10c
Milagrozz <pmontero182004@gmail.com>
2023-03-23 23:54:11 -0500

    login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import LoginForm from '/components/LoginForms';
+import LoginForm from './components/LoginForms';
 
 function App() {
   return (
