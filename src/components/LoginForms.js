50d76f2b499d9e4c6e2d0596cd38f59e5be05967
Milagrozz <pmontero182004@gmail.com>
2023-03-23 23:59:57 -0500

    TA-15-LOGIN

--- src/components/LoginForms.js
+++ src/components/LoginForms.js
@@ -1,31 +1,33 @@
 import React, { useState } from 'react';
 import './LoginForm.css';
 
-function LoginForm() {
-  const [username, setUsername] = useState('');
+const LoginForm = () => {
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(`Username: ${username}, Password: ${password}`);
+    // Aquí podrías agregar la lógica para enviar los datos del formulario al servidor
   };
 
   return (
-    <div className="login-container">
+    <form className="login-form" onSubmit={handleSubmit}>
       <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label htmlFor="username">Username</label>
-          <input type="text" id="username" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+      <label htmlFor="email">Email:</label>
+      <input type="email" id="email" value={email} onChange={handleEmailChange} required />
+      <label htmlFor="password">Password:</label>
+      <input type="password" id="password" value={password} onChange={handlePasswordChange} required />
+      <button type="submit">Submit</button>
+    </form>
   );
-}
+};
 
 export default LoginForm;
